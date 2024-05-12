58cd61a5926dbe8b6099037430d3060ba246c5df
itshswei <itshswei@pm.me>
2024-05-12 18:24:48 +0800

    sec

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Hirsch Ting-Zhong Wei';
+export const SITE_DESCRIPTION = 'my web.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
