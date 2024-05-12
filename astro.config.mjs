c46c74ac68b420a77d69acbc649d6e56cea8b35c
itshswei <itshswei@pm.me>
2024-05-12 23:44:53 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://itshswei.github.io',
-  base: './',
+  base: '.',
   integrations: [mdx(), sitemap(), tailwind()],
   output: 'static'
 });
